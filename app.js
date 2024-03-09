200c10914cb20d34823fb4da49f3b593b21ad1d8
dangnguyen120301 <nguyenduonghaidang42@gmail.com>
2024-03-09 10:18:11 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+const contactsRouter = require("./app/routes/contact.route");
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to contact book application." });
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
 });
-module.express = app;
+app.use("/api/contacts", contactsRouter);
+module.exports = app;
+
+
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const config = require("./app/config");
 
-//start server
+// start server
 const PORT = config.app.port;
-app.Listen(PORT, () => {
-    console.log(`Server is running on port $(PORT), `);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
